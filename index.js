a545c8efc3583f48280ca5f46bf4f55c12f1656a
Andrew Rodrigues Malcher <66445816+AndrewMalcher@users.noreply.github.com>
2024-07-02 14:07:28 +0000

    criação da rota 'oi'

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/oi', (req, res) => {
+  res.send('Olá Mundo!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
